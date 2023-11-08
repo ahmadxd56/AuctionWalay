5499d12f62de180ce6549116b2216ff48c142d07
saleenshady <150207229+saleenshady@users.noreply.github.com>
2023-11-08 03:47:30 -0500

    Update models.py

--- server/api/models.py
+++ server/api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Payments(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add =True)
+    amount = models.IntegerField()
+    status = models.CharField(max_length = 10)
+
+
+class Car(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add =True)
+    mileage = models.IntegerField()
+    make_year = models.IntegerField()
+    mileage = models.IntegerField()
+    status = models.CharField(max_length = 10)
